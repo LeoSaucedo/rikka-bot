88e30c4c71759b5227f251b1db4073d3a599b068
Carlos Saucedo <carlos.gabriel.saucedo@gmail.com>
2018-04-02 19:02:25 -0400

    Gizoogle Update
    Added the initial Gizoogle translator.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # rikka-bot
+A Discord bot that performs various functions.
+
+### Commands
+*Right now, this bot's prefix is* `;`*.*
+- `;hello` - Says hi.
+- `;hello (text)` - translates a given text with gizoogle.
+
+### Requirements
+- bs4
+- requests
+- lxml
+
+### Special Thanks
+- Special thanks to user chafla for his code [gizoogle.py](https://github.com/chafla/gizoogle-py), which helped make this program possible.
+- Hat tip to Rapptz for developing the [Discord API Wrapper](https://github.com/Rapptz/discord.py) used in this program.
---
+++ robot.py
@@ -0,0 +1,43 @@
+"""
+Discord Rikka Bot.
+Carlos Saucedo, 2018
+"""
+
+import discord
+from test.test_asyncgen import asyncio
+import gizoogle
+
+print(gizoogle.text("Hello world"))
+
+#Auth token
+tokenfile = open("auth_token.txt", "r")
+token = tokenfile.read()
+
+#Instantiates Discord client
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    #Makes sure bot does not reply to itself
+    if message.author == client.user:
+        return
+    if message.content.startswith(";hello") or message.content.startswith(";hi"):
+        """Says hi."""
+        msg = "hello {0.author.mention} -chan!".format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith(";gizoogle"):
+        """Gizoogle!"""
+        rawMessage = message.content.replace(";gizoogle ", "")
+        translatedMessage = gizoogle.text(rawMessage)
+        msg = translatedMessage.format(message)
+        await client.send_message(message.channel, msg)
+"""
+Logs into the server.
+"""
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("-------")
+client.run(token)
