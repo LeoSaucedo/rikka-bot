38517056d8c7db324b686d9461c292f2657e9bdf
Carlos Saucedo <carlos.gabriel.saucedo@gmail.com>
2018-04-03 11:19:00 -0400

    The Status Update
    Added presence measuring number of joined servers.
    
    Improved code size.

--- robot.py
+++ robot.py
@@ -7,6 +7,7 @@ import discord
 import asyncio
 import gizoogle
 from random import randint
+from discord import message
 
 #Auth token
 tokenfile = open("auth_token.txt", "r")
@@ -21,65 +22,81 @@ hugcount = len(huglist) - 1 # -1 to compensate for array lengths.
 #Instantiates Discord client
 client = discord.Client()
 
+#Discord prefix
+prefix = ";"
+
+def command(string):
+    #Builds a command out of the given string.
+    return prefix + string
+
+def getArgument(command, message):
+    #Gets the argument text as a string.
+    argument = message.content.replace(command + " ", "")
+    return argument
+    
 @client.event
 async def on_message(message):
     #Makes sure bot does not reply to itself
     if message.author == client.user:
         return
-    if message.content.startswith(";hello") or message.content.startswith(";hi"):
-        """Says hi."""
+    if message.content.startswith(command("hello")) or message.content.startswith(command("hi")):
+        #Says hi and embeds a gif, mentioning the author of the message.
         msg = "h-hello {0.author.mention}-chan! ".format(message) + 'https://cdn.discordapp.com/attachments/402744318013603840/430592483282386974/image.gif'
         await client.send_message(message.channel, msg)
-    if message.content.startswith(";gizoogle"):
-        """Gizoogle!"""
-        rawMessage = message.content.replace(";gizoogle ", "")
-        translatedMessage = gizoogle.text(rawMessage)
+    if message.content.startswith(command("gizoogle")):
+        #Gizoogles the given string and returns it.
+        translatedMessage = gizoogle.text(getArgument(command("gizoogle"), message))
         msg = "{0.author.mention} says: ".format(message) + translatedMessage.format(message)
         await client.send_message(message.channel, msg)
         await client.delete_message(message)
-    if message.content.startswith(";hugme"):
-        """Hugs the author of the message."""
+    if message.content.startswith(command("hugme")) or message.content == command("hug"):
+        #Hugs the author of the message.
         msg = "{0.author.mention}: ".format(message) + huglist[randint(0,hugcount)] 
         await client.send_message(message.channel, msg)
-    if message.content.startswith(";hug "):
-        """Hugs a specified user."""
+    if message.content.startswith(command("hug ")):
+        #Hugs the first user mentioned by the author.
         msg = "{0.author.mention} hugs {0.mentions[0].mention}! ".format(message) + huglist[randint(0,hugcount)]
         await client.send_message(message.channel, msg)
         await client.delete_message(message)
+    if "gay" in message.content:
+        #Self explanatory command. Just for the memes.
+        msg = "no u {0.author.mention}".format(message)
+        await client.send_message(message.channel, msg)
         
     """Miscellaneous gifs"""
-    if message.content == ";shocked":
+    if message.content == command("shocked"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430591612637413389/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";smile":
+    if message.content == command("smile"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430591877834735617/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";hentai":
+    if message.content == command("hentai"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430593080215994370/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";blush":
+    if message.content == command("blush"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430593551554969600/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";bdsm":
+    if message.content == command("bdsm"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430593796045144064/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";rekt":
+    if message.content == command("rekt"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430594037427470336/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";boop":
+    if message.content == command("boop"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430594711602987008/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";fuckoff":
+    if message.content == command("fuckoff"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430594846022041601/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";sanic":
+    if message.content == command("sanic"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430595068156575756/image.gif"
         await client.send_message(message.channel, msg)
-    if message.content == ";dreamy":
+    if message.content == command("dreamy"):
         msg = "https://cdn.discordapp.com/attachments/402744318013603840/430595392669745153/image.gif"
         await client.send_message(message.channel, msg)
+
 """
-Logs into the server.
+Bot login actions
 """
 @client.event
 async def on_ready():
@@ -88,4 +105,7 @@ async def on_ready():
     print(client.user.id)
     print("-------")
     print("loaded hugs: " + str(hugcount + 1)) # +1 because humans are not computers.
-client.run(token)
+    serversConnected = str(len(client.servers))
+    print("Guilds connected: " + serversConnected)#Returns number of guilds connected to
+    await client.change_presence(game=discord.Game(name='in ' + serversConnected + ' servers!'))
+client.run(token) #runs the bot.
