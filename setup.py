a1af8fedb4f2b92290048212499bc4cd3672e696
Carlos Saucedo <carlos.gabriel.saucedo@gmail.com>
2018-04-06 10:55:37 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(name = 'rikka-bot',
+    version='0.1',
+    description='A discord bot that performs various functions.',
+    url='https://github.com/LeoSaucedo/rikka-bot',
+    author='LeoSaucedo',
+    author_email='carlos@cgsphoto.com',
+    license='GPL-3.0',
+    packages=['rikka-bot'],
+    install_requires=[
+        'discord',
+        'asyncio',
+        'googletrans',
+        'bs4',]
+    )
