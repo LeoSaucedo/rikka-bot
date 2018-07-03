5e0b4d3a171f2f7c7236fc0d4414269c0538d6d9
Carlos Saucedo <carlos@cgsphoto.com>
2018-07-03 15:14:49 -0500

    Add Bee Movie Extraction script.
    Added initial *UNTESTED* code for bee movie quote extraction.

--- Mods/beemovie.py
+++ Mods/beemovie.py
@@ -1,1 +1,17 @@
+import urllib2
+from bs4 import BeautifulSoup
 
+def getQuote():
+  try:
+    # query the website and return the html to the variable ‘page’
+    quote_page = "http://wizardlywonders.xyz:3054/"
+    page = urllib2.urlopen(quote_page)
+
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, ‘html.parser’)
+
+    # Extract the body of the website, in this case the quote.
+    quote = soup.find('body').text.strip()
+    return quote
+  except exception as e:
+    print("Failed to load quote.")
