fcc6ba450a1fa25c1b0cbc317df5d85cf6062a28
Carlos Saucedo <carlos@cgsphoto.com>
2018-07-03 22:17:48 -0500

    Removed urllib2.
    Removed urllib2 and replaced it with requests.
    
    Fixed, finalized, and tested code.

--- Mods/beemovie.py
+++ Mods/beemovie.py
@@ -1,17 +1,17 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 
 def getQuote():
   try:
     # query the website and return the html to the variable ‘page’
     quote_page = "http://wizardlywonders.xyz:3054/"
-    page = urllib2.urlopen(quote_page)
+    page = requests.get(quote_page)
 
     # parse the html using beautiful soup and store in variable `soup`
-    soup = BeautifulSoup(page, ‘html.parser’)
+    soup = BeautifulSoup(page.content, "html.parser")
 
     # Extract the body of the website, in this case the quote.
-    quote = soup.find('body').text.strip()
+    quote = str(soup)
     return quote
-  except exception as e:
+  except Exception as e:
     print("Failed to load quote.")
