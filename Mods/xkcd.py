fecb722ec8bc9f8c082807e9ad4f141e47db9b92
Tomo-Nyan <tomo@katter.club>
2018-07-09 14:44:38 +0100

    added  to actually fetch the latest comic for each function, added , and fixed date formatting to comply to ISO 8601.

--- Mods/xkcd.py
+++ Mods/xkcd.py
@@ -2,29 +2,24 @@ import urllib.request, json
 from random import randint
 from discord import Embed
 
-# Feteches the JSON of the latest submission.
-with urllib.request.urlopen("https://xkcd.com/info.0.json") as url:
-    data = json.load(url)
-   
-latestComicID = data["num"]
+def getLatestComicData():
+    # Feteches the JSON of the latest submission.
+    with urllib.request.urlopen("https://xkcd.com/info.0.json") as url:
+        data = json.load(url)
+        return data
 
 def getLatestComic():
+    data = getLatestComicData()
+    
     imgsrc =  data["img"]
-    imgtitle = data["title"]
-    imgdate = data["month"]+"/" + data["day"] + "/" + data["year"]
-    imgalt = data["alt"]
     
-    e = Embed(color=0x7610ba)
-    e.set_image(url=imgsrc)
-    e.set_footer(text=imgalt)
-    e.add_field(name=imgtitle, value=imgdate, inline=False)
-    
-    return e
+    return _formatComic(data,imgsrc)
     
 def getRandomComic():
+    latest = getLatestComicData()
     
     # Generates a random number to represent the ComicID.
-    randomComicID = randint(1, latestComicID)
+    randomComicID = randint(1, latest["num"])
     
     
     # Fetches the JSON and returns the image.
@@ -32,36 +27,32 @@ def getRandomComic():
         data = json.load(randomUrl)
         
     imgsrc =  data["img"]
-    imgtitle = data["title"]
-    imgdate = data["month"]+"/" + data["day"] + "/" + data["year"]
-    imgalt = data["alt"]
-    
-    e = Embed(color=0x7610ba)
-    e.set_image(url=imgsrc)
-    e.set_footer(text=imgalt)
-    e.add_field(name=imgtitle, value=imgdate, inline=False)
-    
-    return e
+
+    return _formatComic(data,imgsrc)
     
 def getComic(comicID):
+    latest = getLatestComicData()
     
     e = None
     
-    if comicID > 1 and comicID <= latestComicID:
+    if comicID > 1 and comicID <= latest["num"]:
         # Comic ID is valid.
         
         with urllib.request.urlopen("http://xkcd.com/" + str(comicID) + "/info.0.json") as comicUrl:
             data = json.load(comicUrl)
             
             imgsrc =  data["img"]
-            
-        imgtitle = data["title"]
-        imgdate = data["month"]+"/" + data["day"] + "/" + data["year"]
-        imgalt = data["alt"]
-        
-        e = Embed(color=0x7610ba)
-        e.set_image(url=imgsrc)
-        e.set_footer(text=imgalt)
-        e.add_field(name=imgtitle, value=imgdate, inline=False)
     
+    return _formatComic(data,imgsrc)
+
+def _formatComic(data,imgsrc):
+    imgtitle = data["title"]
+    imgdate = "".join((data["year"],"-",data["month"],"-",data["day"]))
+    imgalt = data["alt"]
+    
+    e = Embed(color=0x7610ba)
+    e.set_image(url=imgsrc)
+    e.set_footer(text=imgalt)
+    e.add_field(name=imgtitle, value=imgdate, inline=False)
+        
     return e
