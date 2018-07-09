0a6029068011bfb4d4d518c171c9919cf45ec0da
Carlos Saucedo <carlos.gabriel.saucedo@gmail.com>
2018-07-09 15:33:27 -0500

    Corrected casting error.

--- Mods/xkcd.py
+++ Mods/xkcd.py
@@ -63,6 +63,6 @@ def _formatComic(data,imgsrc):
     e = Embed(color=0x7610ba)
     e.set_image(url=imgsrc)
     e.set_footer(text=imgalt)
-    e.add_field(name=imgtitle, value=imgdate + " | " + imgnum, inline=False)
+    e.add_field(name=imgtitle, value=imgdate + " | " + str(imgnum), inline=False)
         
     return e
