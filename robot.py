239c67809054041091293a3e98c9f7ad12fbfcb6
Carlos Saucedo <carlos@cgsphoto.com>
2018-12-21 14:13:45 -0500

    Fixed urllib requests.

--- robot.py
+++ robot.py
@@ -2,7 +2,8 @@
 Discord Rikka Bot.
 Carlos Saucedo, 2018
 """
-import os, discord, string, dbl, json, urllib.request, time
+#TODO: Lint
+import os, discord, string, dbl, json, urllib.request as urllib, time
 import Mods.gizoogle as gizoogle
 from random import randint
 from googletrans import Translator
@@ -122,7 +123,7 @@ def getRawArgument(command, message):
 
 def fetchBooruPost(postID):
     try:
-        with urllib.request.urlopen("".join(map(str,("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&id=",postID)))) as req:
+        with urllib.urlopen("".join(map(str,("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&id=",postID)))) as req:
             data = json.load(req)
         if len(data) > 0:
             post = data[0]
@@ -713,7 +714,7 @@ async def on_message(message):
         if message.channel.is_nsfw() == True:
             cmd = message.content.lstrip(command("gelbooru",message))
             args = cmd.lstrip(cmd.split(" ")[0]).lstrip(" ")
-            with urllib.request.urlopen("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1") as req:
+            with urllib.urlopen("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1") as req:
                 data = json.load(req)
             embed = discord.Embed(color=0xff0000,title="Error",description="baaka, you need to specify a subcommand. desu.")
             if args == "random":
@@ -725,7 +726,7 @@ async def on_message(message):
             if args.startswith("tags"):
                 tags = args.split(" ")[1].split(",")
                 try:
-                    with urllib.request.request.urlopen("".join(("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=","+".join(map(str,tags))))) as req:
+                    with urllib.urlopen("".join(("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=","+".join(map(str,tags))))) as req:
                         data = json.load(req)
                     post = data[randint(0,len(data)-1)]["id"]
                     embed = fetchBooruPost(post)
