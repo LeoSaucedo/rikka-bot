1bd9b1234f30eb45ca84b80c0f1a1e1f8d461a94
Carlos Saucedo <carlos@cgsphoto.com>
2019-05-30 08:36:23 -0400

    Added colleciton date to database migration

--- db/migratedb.py
+++ db/migratedb.py
@@ -1,8 +1,19 @@
 import sqlite3
+import datetime
 
 conn = sqlite3.connect("database.db")
 
 c = conn.cursor()
+
+
+c.execute('''
+CREATE TABLE leaderboard(
+    server varchar(20),
+    user varchar(20),
+    score int,
+    collectionDate text
+)
+''')
 
 leaderboardFile = open("../leaderboard.txt")
 leaderboardList = leaderboardFile.read().splitlines()
@@ -11,7 +22,7 @@ leaderboardFile.close()
 for line in leaderboardList:
     splitline = line.split()
     c.execute('''INSERT INTO leaderboard
-    VALUES ('''+ splitline[0] + ", " + splitline[1] + ", " + splitline[2]+");")
+    VALUES ('''+ "'" + splitline[0] + "', '" + splitline[1] + "', '" + splitline[2]+"', '" + str(datetime.datetime.now().isoformat()) +"');")
 
 conn.commit()
 conn.close()
