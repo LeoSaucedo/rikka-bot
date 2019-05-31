f9e0d11840137837ababd2898504cfc1ed7076c7
Carlos Saucedo <carlos@cgsphoto.com>
2019-05-31 10:27:51 -0400

    Economy SQL Fix Corrected an SQL query error. Updated .gitignore to ignore vscode folder.

--- Mods/economy.py
+++ Mods/economy.py
@@ -16,21 +16,29 @@ def hasCollectedToday(userID):
     c = conn.cursor()
 
     c.execute("SELECT collectionDate FROM leaderboard WHERE user='" + str(userID) + "';")
-    lastCollected = datetime.datetime.strptime(c.fetchone()[0], "%Y-%m-%dT%H:%M:%S.%f")
-    if(datetime.datetime.date(lastCollected) == datetime.datetime.date(datetime.datetime.today())):
-        conn.close()
-        return True
-    else:
-        conn.close()
+    if(c.fetchone() == None):
         return False
-    conn.close()
+    else:
+        lastCollected = datetime.datetime.strptime(c.fetchone()[0], "%Y-%m-%dT%H:%M:%S.%f")
+        if(datetime.datetime.date(lastCollected) == datetime.datetime.date(datetime.datetime.today())):
+            conn.close()
+            return True
+        else:
+            conn.close()
+            return False
+        conn.close()
             
 def setCollectionDate(userID):
     conn = sqlite3.connect("db/database.db")
     c = conn.cursor()
-
-    c.execute("UPDATE leaderboard\n"+
-        "SET collectionDate = " + str(datetime.datetime.now().isoformat()) + "\n" +
-        "WHERE user='" + str(userID) + "';")
+    # print(datetime.date.now().isoformat())
+    # c.execute("UPDATE leaderboard\n"+
+    #     "SET collectionDate = '" + str(datetime.datetime.now().isoformat()) + "'\n" +
+    #     "WHERE user='" + str(userID) + "';")
+    c.execute('''
+    UPDATE LEADERBOARD
+    SET collectionDate=?
+    WHERE USER=?
+    ''', (str(datetime.datetime.now().isoformat()), str(userID)))
     conn.commit()
     conn.close()
