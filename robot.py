b776ea7a08583a39f50a8e20196c555d385aadcf
Carlos Saucedo <carlos@cgsphoto.com>
2019-06-04 10:19:36 -0400

    Improved performance for role searching.

--- robot.py
+++ robot.py
@@ -857,9 +857,10 @@ async def on_message(message):
                 await message.channel.guild.create_role(color=color, name=("Color - " + colorName))
                 newRole = None
                 statusMsg(message.channel.guild.roles)
-                for role in message.channel.guild.roles:
-                    if(role.name == ("Color - " + colorName)):
-                        newRole = role
+                while newRole == None:
+                    # Keeps searching until the role is found, since it was created.
+                    statusMsg("Searching for role " "Color - " + colorName + "." , 1)
+                    newRole = discord.utils.get(message.guild.roles, name=("Color - " + colorName))
                 # Place the role directly under the bot's top role position.
                 await newRole.edit(position=(message.channel.guild.me.top_role.position-1))
                 statusMsg(message.channel.guild.roles)
