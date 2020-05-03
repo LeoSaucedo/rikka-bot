3bf1c6429edc9a61d0e99ac56133cbd34d069021
Carlos Saucedo <carlos@cgsphoto.com>
2020-05-02 23:02:22 -0400

    Added construction page.

---
+++ construction.py
@@ -0,0 +1,26 @@
+"""Under construction dialog.
+"""
+
+import discord
+from discord.ext import commands
+import json
+
+bot = commands.AutoShardedBot(command_prefix=";")
+
+
+@bot.event
+async def on_ready():
+    """Runs when the bot has started.
+    """
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(discord.Game(name="Under construction..."))
+
+if __name__ == "__main__":
+
+    # Load bot token and run bot.
+    with open("json/config.json", "r") as h:
+        config = json.load(h)
+        bot.run(config["token"])
