5ef21f69ca377277aea8209b8bca1d5a7452ca62
Carlos Saucedo <carlos@cgsphoto.com>
2020-05-21 23:43:15 -0400

    Aded 8ball feature.

---
+++ Cogs/Casino.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+from random import randint
+
+
+class Casino(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="8ball")
+    async def eightball(self, ctx):
+        """Gets a message from the eight-ball.
+
+        Args:
+            ctx (discord.ext.Context): The message Context.
+        """
+        responses = [
+            "it is certain.",
+            "it is decidedly so.",
+            "without a doubt.",
+            "yes, definitely.",
+            "you may rely on it.",
+            "you can count on it.",
+            "as I see it, yes.",
+            "most likely.",
+            "outlook good.",
+            "yes.",
+            "signs point to yes.",
+            "absolutely.",
+            "reply hazy, try again.",
+            "ask again later.",
+            "better not tell you now.",
+            "cannot predict now.",
+            "concentrate and ask again.",
+            "don't count on it.",
+            "my reply is no.",
+            "my sources say no.",
+            "outlook not so good.",
+            "very doubtful.",
+            "chances aren't good."
+        ]
+        response = ("{0.author.mention}, " +
+                    responses[randint(0, len(responses)-1)]).format(ctx.message)
+        embed = discord.Embed(color=0x8000ff)
+        embed.set_author(
+            name="Magic 8-Ball",
+            icon_url="https://emojipedia-us.s3.amazonaws.com/thumbs/120/twitter/134/billiards_1f3b1.png",
+        )
+        embed.add_field(
+            name="Prediction:",
+            value=response,
+            inline=False
+        )
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Casino(bot))
