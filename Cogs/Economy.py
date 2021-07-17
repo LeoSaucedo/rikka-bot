3e03adfe7a3ab158d8b86b5c0d6cfb3996c5820d
Carlos Saucedo <carlos@cgsphoto.com>
2021-07-17 00:36:53 -0400

    Add global leaderboard.

--- Cogs/Economy.py
+++ Cogs/Economy.py
@@ -11,11 +11,39 @@ class Economy(commands.Cog):
 
   @commands.command()
   async def score(self, ctx):
+    """Gets the amount of money you have."""
     if(len(ctx.message.mentions) == 0):
       user = ctx.message.author
     else:
       user = ctx.message.mentions[0]
     await ctx.send("`" + str(user.name) + "`'s score is: " + str(getScore(user.id)))
+
+  @commands.command()
+  async def leaderboard(self, ctx):
+    """Gets the leaderboard."""
+    conn = sqlite3.connect("db/database.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM leaderboard ORDER BY score DESC")
+    data = c.fetchall()
+    conn.close()
+    if(len(data) == 0):
+      await ctx.send("There is no one on the leaderboard.")
+    else:
+      msg = ""
+      i = 0
+      while(i < 9):
+        try:
+          user = await self.bot.fetch_user(data[i][1])
+          msg += str(i+1) + ": " + "`" + str(user.display_name) + \
+              "` - " + str(data[i][2]) + "\n"
+          i += 1
+        except:
+          pass
+      embed = discord.Embed(title="Top 10 Globally:",
+                            description=msg, color=0x12f202)
+      embed.set_author(
+          name="Leaderboard", icon_url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/money-bag_1f4b0.png")
+      await ctx.send(embed=embed)
 
 
 async def addPoints(serverID, userID, amount):
